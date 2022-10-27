02dd5f6e32e35d863a376adfebe919fbf50927eb
Lukasz Polon <lukaspolon@gmail.com>
2022-10-27 09:01:46 +0200

    Sixth update

--- setup.py
+++ setup.py
@@ -11,21 +11,8 @@ if sys.version_info < MIN_PYTHON:
     )
 
 
-def get_requirements() -> List[str]:
-    """ Get all Python modules required by application to be installed.
-        Returns:
-            modules: list of Python libs to install
-    """
-    requirements_file_path = "requirements.txt"
-    with open(requirements_file_path, "r", encoding="UTF-8") as req_file:
-        modules = req_file.readlines()
-    modules = [module.strip() for module in modules if module]
-    return modules
-
-
 setup(
     name="ApiWordCounterTest",
-    install_requires=get_requirements(),
     author='Lukasz Polon',
     author_email='lukaspolon@gmail.com',
     packages=find_packages(),
